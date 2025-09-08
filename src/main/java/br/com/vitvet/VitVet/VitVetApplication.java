0980a1f7f93a2ae69e4d850e030aa0b174d43d6d
Márcio Silva <marcio.guilherme.vieira.silva@gmail.com>
2025-09-08 19:56:36 -0300

    First commit

---
+++ src/main/java/br/com/vitvet/VitVet/VitVetApplication.java
@@ -0,0 +1,13 @@
+package br.com.vitvet.VitVet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitVetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitVetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vitvet/VitVet/VitVetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vitvet.VitVet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitVetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
