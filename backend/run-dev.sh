4c90181e64ac57288b87fcc51ab0bd0be7f53773
Márcio Silva <marcio.guilherme.vieira.silva@gmail.com>
2025-11-10 20:32:15 -0300

    mudancas no pom e script para execucao com perfil de dev

--- backend/run-dev.sh
+++ backend/run-dev.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# Este script inicia a aplicação Spring Boot com o perfil 'dev' ativo.
 
 echo "Iniciando a aplicação VitVet em modo de DESENVOLVIMENTO..."
 echo "A segurança estará DESABILITADA."
 
-./mvnw spring-boot:run -Dspring-boot.run.profiles=dev
+./mvnw spring-boot:run -e -X -Dspring-boot.run.profiles=dev
